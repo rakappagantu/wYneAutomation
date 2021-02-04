c5896e1c97405252604348ed548e7bda0d67b8be
raghuram <raghuram.kappagantu@gmail.com>
2021-02-04 20:05:41 +0530

    updated aapplication.properties file

---
+++ src/main/java/com/wYne/automation/pages/Loginpage.java
@@ -0,0 +1,17 @@
+package com.wYne.automation.pages;
+
+import com.wYne.automation.ui.elements.SlWebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Loginpage {
+    @FindBy(xpath = "")
+    public SlWebElement login_username_textbox;
+
+    @FindBy(xpath = "")
+    public SlWebElement login_password_textbox;
+
+    @FindBy(xpath = "")
+    public SlWebElement login_button;
+
+
+}
